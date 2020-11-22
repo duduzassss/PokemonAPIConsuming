69679b00044b488d1fef882701cd1e298f4e7923
Eduardo Bitencourt <eduardo.pbitencourt@hotmail.com>
2020-11-22 19:45:05 -0300

    Update routes, css pokemon/pokemons

--- app/controllers/pokemons/routes.py
+++ app/controllers/pokemons/routes.py
@@ -8,42 +8,49 @@ from app.models.models import ImagePokemon
 
 from string import Template # importação do join
 from sqlalchemy import text
+import time
 
+
+def conectAPI():
+    requisicao = requests.get("https://www.canalti.com.br/api/pokemons.json")
+    resposta = json.loads(requisicao.text) # Transform in dict iterable
+    
+    return resposta
 
 @app.route('/pokemons')
 def pokemons():
-    lista_nome = []
-    lista_url = []
-    # for j in range(0,6):
-    requisicao = requests.get("https://pokeapi.co/api/v2/pokemon?limit=50") #1050
-    resposta = json.loads(requisicao.text) # Transform in dict iterable
-    pokemon  = resposta["results"]
-        
-    dados = []
-    for p in pokemon:
-        req = requests.get(p["url"])
-        res = json.loads(req.text)
-        poke = res["sprites"]["other"]["official-artwork"]["front_default"]
-        dicio = {"name": p["name"], "img": poke}
-        dados.append(dicio)
-        # print(dados[0]["name"])
-        
-        
-    for i in range(0,len(dados)):
-        # print(dados[i])
-        p = ImagePokemon.query.filter_by(name=dados[i]["name"]).first()
-        if p is None:
-            save_pokemon = ImagePokemon(
-                            name = dados[i]["name"],
-                            link_img = dados[i]["img"]
-            )
-            db.session.add(save_pokemon)
-            db.session.commit()
+    resposta = conectAPI()
+    pokemons  = resposta["pokemon"]
+    
+   
+    lista_pokemons = []
+    for pokemon in pokemons:
+        dicio = {"num": pokemon["num"],"name": pokemon["name"],"img": pokemon["img"], "type": pokemon["type"]}
+        lista_pokemons.append(dicio)
             
-    send_pokemons = ImagePokemon.query.all()
-    # print(send_pokemons[0])
+    return render_template('pokemons/pokemons.html', lista_pokemons = lista_pokemons) #send_pokemons = send_pokemons
+
+@app.route('/pokemon/<name>')
+def pokemon(name):
+    resposta = conectAPI()
+    pokemons = resposta["pokemon"]
+    
+    pokemon = str(name).capitalize()
+    #print(pokemon) #bulbasaur
+    
+    try:
+        for poke in pokemons: # return multiples dict
+            if pokemon in poke["name"]:
+                lista = []
+                lista.append(poke)
+                print(lista)
+
+               
+
+                return render_template('pokemons/single_pokemon.html', lista = lista)
+                
+    except:
+        print("Error: Pokémon name is not in API.")
+
     
     
-        
-            
-    return render_template('pokemons/pokemons.html', send_pokemons = send_pokemons) #lista_image = lista_image, lista_nome = lista_nome
